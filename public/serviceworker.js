907d35e1500e8aa2bde707aac35121eff3a52f86
vivek prasad <ppubg7158@gmail.com>
2021-06-23 19:21:02 +0530

    Its now a pwa Application

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -16,7 +16,7 @@ self.addEventListener('install', event => {
 //listen for requests
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(() => {
+    caches.match(event.request).then(async () => {
       return fetch(event.request).catch(() => {
         caches.match('offline.html')
       })
